75e956ef83c3035079dd2e1e1da62b6de0fdc0e4
shutang <shutang.yu@crypto.com>
2024-07-17 19:17:13 -0500

    fix: special handling of `this` in each block

--- lib/zappa/sendgrid/variable.ex
+++ lib/zappa/sendgrid/variable.ex
@@ -1,6 +1,8 @@
 defmodule Zappa.Sendgrid.Variable do
   @moduledoc false
 
+  # this is a special variable referenced in each block helper
+  def from_tag_name("this"), do: {:ok, "this"}
   def from_tag_name(tag_name), do: {:ok, "@#{normalize(tag_name)}"}
 
   def from_options_arg(%{value: value, quoted?: quoted?}) do
